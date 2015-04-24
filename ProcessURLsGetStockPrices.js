9b46de91c4138ebbc5edc54b4e09efe95e6fdffb
pedmunds <paul.edmunds@talk21.com>
2015-04-24 09:16:28 +0100

    bugfix
    updates to deal with lead errors

--- ProcessURLsGetStockPrices.js
+++ ProcessURLsGetStockPrices.js
@@ -2,15 +2,15 @@
 
 /*get list of share isins from file
 get stock data from a url like this:
-http://www.google.co.uk/finance/historical?q=LON:VLX&startdate=1-Apr-15&enddate=2-Apr-15&output=csv
+http://www.google.co.uk/finance/historical?q=LON:888&startdate=31-Oct-12&enddate=&output=csv
 output parsed details to shares.txt file for database import
 */
 
 // enter stat and end dates for historic data
-var startdate= '1-Jan-15'
+var startdate= '31-Oct-12'
 var enddate='' //blank = today
 var shareArray = [];
-var epic = '';
+
 
 fs = require('fs')
 fs.appendFile('shares.txt', 'EPIC,DATE,CLOSE\n', function (err) {});
@@ -24,17 +24,16 @@ fs.readFile('epicsfile.txt', 'utf8', function (err,data) {
 
 function logArrayElements(element, index, array) {
   console.log('a[' + index + '] = ' + element);
-  epic = element;
-shareDataGrabber('http://www.google.co.uk/finance/historical?q=LON:'+element+'&startdate='+startdate+'&enddate='+enddate+'&output=csv');
+shareDataGrabber('http://www.google.co.uk/finance/historical?q=LON:'+element+'&startdate='+startdate+'&enddate='+enddate+'&output=csv', element);
 }
 
-function shareDataGrabbertest(url)
+function shareDataGrabbertest(url, epic)
 {
 			var share=url.toString();			
 			console.log(share);
 }
 
-function shareDataGrabber(url)
+function shareDataGrabber(url, epic)
 {
 /* function to grab share data and write to a file for data import */
 
@@ -47,7 +46,7 @@ request(url, function (error, response, body) {
     if (!error) {
         parsecsv(body);
     } else {
-        console.log(error);
+        console.log("no csv " + error);
     }
 });
 
@@ -58,14 +57,17 @@ var parsecsv = function(allText) {
 	var fs = require('fs');
     //console.log(allText);
     var allTextLines = allText.split(/\r\n|\n/);
+	var line = '';
 
     for (var i=1; i<allTextLines.length-1; i++) {
         var data = allTextLines[i].split(',');
 		// get date plus close
-		var line = epic + ',' + data[0] + ',' + data[4] + '\n';
-//console.log(line);
-	fs.appendFile('shares.txt', line, function (err) {});
+		line = line + epic + ',' + data[0] + ',' + data[4] + '\n';
     }
+//console.log(line);
+fs.appendFile('shares.txt', line, function (err) {console.log(err);});
+
+
     }
 
 };
